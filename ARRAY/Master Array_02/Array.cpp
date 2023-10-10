77eb2cdf0057fb1e198207f6fd3d3ac7066cda56
vinayVishwakarmaa <vinaykumar843569@gmail.com>
2023-10-10 14:52:27 +0530

    Array

---
+++ ARRAY/Master Array_02/Array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+      //Reverse Array
+
+//       int arr[6] = {1, 2, 3, 4, 5};
+
+//       int start = 0, end = 4;
+// //       Start = i end =j
+//       while(start<end){
+//             swap(arr[start],arr[end]);
+//             start++;
+//             end--;
+//       }
+//       for (int i = 0; i < 5;i++)
+//             cout << arr[i]<<" ";
+
+
+
+         
+
+
+
+
+}

668fd4b7516ad1c132e650aae09f99a7318ccd51
vinayVishwakarmaa <vinaykumar843569@gmail.com>
2023-10-12 17:36:10 +0530

    Selection Sort

---
+++ ARRAY/Selection Sort/SelectionSort.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+      // int arr[6] = {10, 4, 1, 3, 2, 7};
+        int arr[1000];
+        int n;
+        cout << "Enter the size of array: ";
+        cin >> n; // Size of array
+        cout << "Enter the element in array: ";
+        for (int i = 0; i < n;i++)
+              cin>> arr[i];
+
+      //         for (int i = 0; i < 5; i++)
+      //         {
+      //               int index = i;
+
+      //               for (int j = i + 1; j < 6; j++)
+      //               {
+      //                     if (arr[j] < arr[index])
+      //                           index = j;
+      //               }
+      //               swap(arr[i], arr[index]);
+      //         }
+      //   for (int i = 0; i < 6;i++)
+      // {
+      //       cout << arr[i]<<" ";
+      // }
+
+      //      Second Solution
+
+                 for (int i = 0; i < n-1; i++)
+              {
+                    int index = i;
+
+                    for (int j = i + 1; j < n; j++)
+                    {
+                          if (arr[j] < arr[index])
+                                index = j;
+                    }
+                    swap(arr[i], arr[index]);
+              }
+        for (int i = 0; i < n;i++)
+      {
+            cout << arr[i]<<" ";
+      }
+
+
+
+      // Auxilarry Space = o(1)
+      // Total Space  = o(n)
+      // Time Compelexity = o(nSqure2)
+}
